4b76a6ccb801e9ad301febf53b0b5971e505eb13
chetan22 <ck.chetan20@gmail.com>
2019-11-12 17:51:02 +0530

    Added more on decorators

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -15,11 +15,62 @@ def decorator_name(func):
 """
 
 import functools
-import timeit
+import time
+
+PLUGINS = {}
 
 def timer(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        exec_time = timeit.timeit(func(*args, **kwargs))
-        print(f"Fininshed {func.__name__} in {exec_time: .4f} secs")
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        print(f'Finished {func.__name__} in {end_time-start_time:4f} secs')
+        return value
+    return wrapper
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_list = [repr(a) for a in args]
+        kwargs_list = [f'{k}={v}' for k, v in kwargs.items()]
+        signature_list  = ', '.join(args_list + kwargs_list)
+        print(f'Calling {func.__name__}({signature_list})')
+        value = func(*args, **kwargs)
+        print(f'{func.__name__} returned {value}')
+        return value
+    return wrapper
+
+def register(func):
+    """Registers a function as a plug-in"""
+    PLUGINS[func.__name__] = func
+    return func
+
+# The way it works:
+# decorator("arg1", "arg2")(func)(*args, **kwargs)
+
+def repeat(num_times=2):
+    def decorator_func(func):
+        @functools.wraps(functools)
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                value = func(*args, **kwargs)
+            return value
+        return wrapper
+    return decorator_func
+    
+# class based decorators
+def singleton(cls):
+    """
+    To ensure that a class has only one instance all the time.
+    This is frequently used in objects like None, True, False
+    in Python
+    """
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs):
+        print('inside wrapper')
+        if not wrapper.instance:
+            wrapper.instance = cls(*args, **kwargs)
+        return wrapper.instance
+    wrapper.instance = None
     return wrapper
--- Decorators/main.py
+++ Decorators/main.py
@@ -1,7 +1,13 @@
+# importing requires modules
+import argparse
+import math
+import functools
 # importing decorators
 from test_decorators import (greetings, greet_twice, greet_someone,
                         greet_someone_rudely, retain_decorated_func_ref,
-                        test_decorated_func_ref,)
+                        test_decorated_func_ref)
+from decorators import (PLUGINS, timer, debug, register, repeat, singleton,)
+
 
 @greetings
 def say_hello():
@@ -24,13 +30,81 @@ def no_hello(name):
     """
     print('I wanted to say hello.. :(')
 
+@timer
+def counter():
+    square_roots = []
+    for _ in range(1000):
+        square_roots.append(math.sqrt(_))
+    return square_roots
 
 
-def main():
+
+@debug
+def approximate_e(terms=10):
+    return sum(1 / math.factorial(n) for n in range(terms))
+
+@register
+def just_a_func():
+    pass
+
+@register
+def just_a_func_with_args(dummy_arg, dummy_kwarg=0):
+    pass
+
+# The way it works:
+# repeat_n_times = repeat(num_times)(repeat_n_times)(name)
+@repeat(num_times=4)
+def repeat_n_times(name='Charles'):
+    print(f'Hello, {name}')
+
+# The way it works:
+# class_obj = sigleton(ClassBaseDecorator)
+@singleton
+class ClassBasesDecorator:
+    pass
+
+@functools.lru_cache()
+def fibonacci(num):
+    if num < 2:
+        return num
+    return fibonacci(num-2) + fibonacci(num-1)
+
+##################### Execution ######################
+
+# creating parser
+parser = argparse.ArgumentParser(allow_abbrev=False)
+
+parser.add_argument('module', metavar='Module', default='main',
+                    help='Specify main or test to run either main or'
+                    'test set of decorators')
+args = parser.parse_args()
+
+def test_main():
     say_hello()
     say_hello_2()
     hello_name('Bob')
     no_hello('Charlie')
 
+def main():
+    counter()
+    print(approximate_e())
+    print(approximate_e(5))
+    just_a_func()
+    just_a_func_with_args(10)
+    print(PLUGINS)
+    repeat_n_times('Marley')
+    repeat_n_times()
+    print('Creating first object')
+    class_obj_1 = ClassBasesDecorator()
+    print('Creating second object')
+    class_obj_2 = ClassBasesDecorator()
+    print(id(class_obj_1) == id(class_obj_2))
+    print(class_obj_1, ' ', class_obj_2)
+    print(fibonacci(10))
+
+
 if __name__ == "__main__":
-    main()
+    if 'test' in args.module:
+        test_main()
+    else:
+        main()
